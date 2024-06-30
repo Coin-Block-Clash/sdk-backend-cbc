b87560bcc28965dc50fb5e78e3685538d3a5e93c
0xabin <abinbvinod@gmail.com>
2024-06-30 22:52:05 +0530

    EC2 CI CD Setup

--- src/routes/handlers/root.ts
+++ src/routes/handlers/root.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 
 export function getRoot(req :Request , res : Response) {
-    res.send([]);
+    res.send(["Working  EC2"]);
 }
 
 export function getRootData(req :Request , res : Response) {
